e45805460dbcb3ba734cac1ba62feceeb854167c
jenisnmm <143386018+jenisnmm@users.noreply.github.com>
2023-11-11 22:20:40 +0530

    updated UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import LeaderboardCard from './components/LeaderboardCard'
 function App() {
   return (
     <>
-      <h1 className="text-5xl mb-10 font-orbitron font-semibold">Team Leaderboard</h1>
+      <h1 className="text-5xl my-8 font-orbitron font-semibold drop-shadow-[0_0_5px_#7d03bb]">
+        TechWiz Leaderboard
+      </h1>
       <LeaderboardCard />
     </>
   );
